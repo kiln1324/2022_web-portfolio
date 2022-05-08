18ffa6d368f1bd0fa06be79316e4985ef32f5ba5
kiln1324 <kiln1324@gmail.com>
2022-05-08 19:34:59 +0900

    test instead eval();
    처음 결과값을 낼 때 사용했던 eval을 대신할 함수 테스트

---
+++ projects/script-demo/js/instead-eval-test.js
@@ -0,0 +1,79 @@
+const number = [1, 2, 3, 4, 5];
+const operators = ["+", "*", "/", "+"];
+console.log(`초기 숫자 : ${number}`);
+console.log(`초기 배열 : ${operators}`);
+const multiply = operators.indexOf("*");
+//let countOfMultiply = operators.filter((element) => "*" === element).length;
+//console.log(`곱하기 연산자 갯수 : ${countOfMultiply}`);
+
+const division = operators.indexOf("/");
+//let countOfDivision = operators.filter((element) => "/" === element).length;
+//console.log(`나누기 연산자 갯수 : ${countOfDivision}`);
+
+const add = operators.indexOf("+");
+//let countOfAdd = operators.filter((element) => "+" === element).length;
+//console.log(`더하기 연산자 갯수 : ${countOfAdd}`);
+
+const subtraction = operators.indexOf("-");
+//let countOfSubtraction = operators.filter((element) => "-" === element).length;
+//console.log(`나누기 연산자 갯수 : ${countOfSubtraction}`);
+
+function sliceOperatorArray(index, string) {
+  index = operators.indexOf(string);
+  if (index === -1) {
+    return false;
+  } else {
+    operators.splice(index, 1);
+    console.log(`잘라낸 연산자 배열 : ${operators}`);
+    return operators;
+  }
+}
+/* sliceOperatorArray(multiply, "*");
+sliceOperatorArray(division, "/");
+sliceOperatorArray(add, "+");
+sliceOperatorArray(subtraction, "-"); */
+
+function calculateNumberArray(index, string) {
+  console.log(`인덱스번호 : ${index}`);
+  console.log(`계산 배열 : ${number}`);
+  countOfOperator = operators.filter((element) => string === element).length;
+
+  for (let i = 0; i < countOfOperator; i++) {
+    sliceOperatorArray(index, string);
+    if (index === -1) {
+      return false;
+    } else {
+      if (index !== -1) {
+        switch (string) {
+          case "*":
+            number[index] *= number[index + 1];
+            break;
+          case "/":
+            number[index] /= number[index + 1];
+            break;
+          case "+":
+            number[index] += number[index + 1];
+            break;
+          case "-":
+            number[index] -= number[index + 1];
+            break;
+
+          default:
+            return false;
+        }
+
+        number.splice(index + 1, 1);
+      }
+    }
+  }
+}
+calculateNumberArray(multiply, "*");
+calculateNumberArray(division, "/");
+calculateNumberArray(add, "+");
+calculateNumberArray(subtraction, "-");
+/* if()
+  if (multiply) {
+    number[multiply] = number[multiply] * number[multiply + 1];
+    number.splice(multiply + 1, 1);
+    operators.splice(operators[multiply], 1);
+  } */
