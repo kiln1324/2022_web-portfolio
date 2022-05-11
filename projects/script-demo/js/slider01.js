7d43652b396265e50abb0add25df2b5ef7acebbf
kiln1324 <kiln1324@gmail.com>
2022-05-11 16:02:13 +0900

    slider fix

--- projects/script-demo/js/slider01.js
+++ projects/script-demo/js/slider01.js
@@ -1,27 +1,37 @@
 "use stric";
 
-const countLi = $("slider_wrap ul li").length;
-const liW = parseInt($(".slider_wrap ul li").eq(0).css("width"));
+const countLi = $(".slider_wrap ul li").length;
+let liW = parseInt($(".slider_wrap ul li").eq(0).css("width"));
 const ACTIVE_CLASSNAME = "active";
-console.log(liW);
+let list_zIndex = countLi;
 
+console.log(countLi);
+/* --------------------------준비하기 */
 function organazeLi() {
-  for (let i = 0; i < countLi; i++) {
+  for (let i = countLi - 1; i >= 0; i--) {
     $(".slider_wrap ul li")
       .eq(i)
       .css({
-        zIndex: `1${-i + countLi}`,
+        zIndex: countLi - i,
       });
   }
 }
 
 organazeLi();
 
-/* --------------------------준비하기 */
 $(".slider_wrap ul").prepend($(".slider_wrap ul li:last"));
 $(".slider_wrap ul").css({
   left: `-${liW}px`,
 });
+
+window.addEventListener("resize", function () {
+  liW = parseInt($(".slider_wrap ul li").eq(0).css("width"));
+  $(".slider_wrap ul").css({
+    left: `-${liW}px`,
+  });
+  console.log(`크기변경중.... /// ${liW}`);
+});
+
 let prot = 0;
 /* --------------------------//준비하기 */
 
@@ -29,65 +39,24 @@ function slider() {
   if (prot === 1) return false;
   prot = 1;
 
-  let indexNum = $(".btn-grp button").index(this);
+  let indexNum = $(".btns").index(this);
 
   if (indexNum === 0) {
-    console.log("왼쪽");
-    $(".slider_wrap ul li").eq(1).find(".img-box").css({
-      transform: "translate(-200px)",
-    });
-    $(".slider_wrap ul li").eq(2).find(".img-box").css({
-      transform: "translate(0)",
-    });
-    $(".slider_wrap ul li").eq(3).find(".img-box").css({
-      transform: "translate(-100px)",
-    });
-    $(".slider_wrap ul li").eq(4).find(".img-box").css({
-      transform: "translate(-200px)",
-    });
-    $(".slider_wrap ul li:last").find(".img-box").css({
-      transform: "translate(0)",
-    });
-
-    $(".slider_wrap ul li")
-      .eq(2)
-      .addClass(ACTIVE_CLASSNAME)
-      .siblings()
-      .removeClass(ACTIVE_CLASSNAME);
-
-    setTimeout(() => {
-      $(".slider_wrap ul").animate(
-        {
-          left: `-${liW * 2}px`,
-        },
-        800,
-        function () {
-          $(".slider_wrap ul").append($(".slider_wrap ul li:first"));
-
-          $(".slider_wrap ul").css({
-            //left: -380,
-            left: `-${liW}px`,
-          });
-          prot = 0;
-        }
-      );
-    }, 10);
-  } else {
     console.log("오른쪽");
     $(".slider_wrap ul li").eq(0).find(".img-box").css({
-      transform: "translate(0)",
+      transform: "translate(2vw)",
     });
     $(".slider_wrap ul li").eq(1).find(".img-box").css({
-      transform: "translate(-100px)",
+      transform: "translate(-8vw)",
     });
     $(".slider_wrap ul li").eq(2).find(".img-box").css({
-      transform: "translate(-200px)",
+      transform: "translate(-18vw)",
     });
     $(".slider_wrap ul li").eq(3).find(".img-box").css({
-      transform: "translate(0)",
+      transform: "translate(2vw)",
     });
     $(".slider_wrap ul li:last").find(".img-box").css({
-      transform: "translate(-200px)",
+      transform: "translate(-18vw)",
     });
 
     $(".slider_wrap ul li")
@@ -113,12 +82,53 @@ function slider() {
         }
       );
     }, 10);
+  } else {
+    console.log("왼쪽");
+    $(".slider_wrap ul li").eq(1).find(".img-box").css({
+      transform: "translate(-18vw)",
+    });
+    $(".slider_wrap ul li").eq(2).find(".img-box").css({
+      transform: "translate(2vw)",
+    });
+    $(".slider_wrap ul li").eq(3).find(".img-box").css({
+      transform: "translate(-8vw)",
+    });
+    $(".slider_wrap ul li").eq(4).find(".img-box").css({
+      transform: "translate(-18vw)",
+    });
+    $(".slider_wrap ul li:last").find(".img-box").css({
+      transform: "translate(2vw)",
+    });
+
+    $(".slider_wrap ul li")
+      .eq(2)
+      .addClass(ACTIVE_CLASSNAME)
+      .siblings()
+      .removeClass(ACTIVE_CLASSNAME);
+
+    setTimeout(() => {
+      $(".slider_wrap ul").animate(
+        {
+          left: `-${liW * 2}px`,
+        },
+        800,
+        function () {
+          $(".slider_wrap ul").append($(".slider_wrap ul li:first"));
+
+          $(".slider_wrap ul").css({
+            //left: -380,
+            left: `-${liW}px`,
+          });
+          prot = 0;
+        }
+      );
+    }, 10);
   }
 
   organazeLi();
 } // fnc
 
-$(".btn-grp button").click(slider); //click
+$(".btns").click(slider); //click
 /* -----------------animation-test----------------------------- */
 
 /* --------------------canvas */
